69e2b2b304af5b66ce1c3689ffff8ec3f8ac778c
Fadwa <faaadwaaa05@gmail.com>
2022-03-25 22:34:03 +0100

    style: creating the components (html+css)/ v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
