87b82bea59639425f7239b18d715a1cfba310f77
Fadwa <faaadwaaa05@gmail.com>
2022-03-25 20:59:32 +0100

    initiating the app's components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TasksListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule
